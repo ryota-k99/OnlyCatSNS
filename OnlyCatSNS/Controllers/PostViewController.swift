5380e3081d7887146af7d53a7a151841896025fe
ryota-k99 <ninixi16@gmail.com>
2020-04-08 22:27:15 +0900

    Created new controllers and set UI components on the story board.

---
+++ OnlyCatSNS/Controllers/PostViewController.swift
@@ -0,0 +1,30 @@
+//
+//  PostViewController.swift
+//  OnlyCatSNS
+//
+//  Created by Kato Ryota  on 8/04/20.
+//  Copyright © 2020 Kato Ryota . All rights reserved.
+//
+
+import UIKit
+
+class PostViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ OnlyCatSNS/Controllers/TimeLineViewController.swift
@@ -0,0 +1,29 @@
+//
+//  TimeLineViewController.swift
+//  OnlyCatSNS
+//
+//  Created by Kato Ryota  on 8/04/20.
+//  Copyright © 2020 Kato Ryota . All rights reserved.
+//
+
+import UIKit
+
+class TimeLineViewController: UIViewController,UITableViewDelegate,UITableViewDataSource {
+   
+    
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+           <#code#>
+       }
+       
+       func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+           <#code#>
+       }
+}
